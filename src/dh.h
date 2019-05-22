b0305575896bedab9a9246346a3ba9e314844ea1
suchang <1103232282@qq.com>
2019-05-22 18:45:07 +0800

    初始化

---
+++ src/dh.h
@@ -0,0 +1,8 @@
+//
+// Created by 苏畅 on 2019/5/22.
+//
+
+#ifndef NET_SAFE_DH_H
+#define NET_SAFE_DH_H
+
+#endif //NET_SAFE_DH_H
---
+++ tests/test_dh.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 苏畅 on 2019/5/22.
+//
+
+int main() {
+    return 0;
+}
