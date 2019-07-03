8396fd7e165d9e6689a5f6d4535a1e4f6ec33875
suchang <1103232282@qq.com>
2019-07-03 16:12:12 +0800

    dh 算法完成

---
+++ readne.md
@@ -0,0 +1,3 @@
+# ssl
+
+class SSLHandler(
---
+++ src/SSLHandler.h
@@ -0,0 +1,69 @@
+//
+// Created by 苏畅 on 2019/7/3.
+//
+
+#ifndef NET_SAFE_SSLHANDLER_H
+#define NET_SAFE_SSLHANDLER_H
+
+#include<string>
+
+class  SSLContext {
+private:
+    long  _i_pub; // 个人的共钥匙
+    long _i_pri; // 个人的私钥
+    long _t_pub; // 对方的共钥
+    long _session_key; // 会话密钥
+public:
+    SSLContext(long i_pub, long i_pri, long t_pub) :_i_pri(i_pri), _i_pub(i_pub), _t_pub(t_pub), _session_key(-1)
+    {}; //初始化
+
+    long GetIPub(){
+        return _i_pub;
+    }
+
+    long GetIPri(){
+        return _i_pri;
+    };
+
+    long GetTPub(){
+        return _i_pri;
+    };
+
+    long GetSessionKey() {
+        return _session_key;
+    }
+
+    void SetSessionKey(long key) {
+        _session_key = key;
+    }
+
+};
+
+class SSLHandler {
+
+public:
+    enum SSL_STATE {
+        IDLE,
+        SHAKEHANDS,
+        TRANS,
+        CLOSE
+    };
+private:
+    SSL_STATE _state;
+    SSLContext _ctx;
+
+public:
+    SSLHandler(int socketfd, SSLContext& ctx); // 传入一个链接好的socket进行构造
+
+    int DoShakeHands(); // 进行ssl握手
+    int S_Write(const std::string s); // 写数据 先对数据加密， 然后进行hash  消息格式：  哈希值长度 + '\r\n' + hash值  + \r\n + 加密后的消息 '\r\n\r\n'
+    int S_Read(char *buf, int size); // 先对数据进行校验, 然后取出消息解密
+
+    SSL_STATE GetSSLState() {
+        return _state;
+    }
+
+};
+
+
+#endif //NET_SAFE_SSLHANDLER_H
