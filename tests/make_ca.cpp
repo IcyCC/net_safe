340410ddbdb0fca6e3eb7a0c8df21381453635b8
zhandong <1468767640@qq.com>
2019-07-04 19:39:02 +0800

    merge 1

--- tests/make_ca.cpp
+++ tests/make_ca.cpp
@@ -1,4 +1,36 @@
 //
 // Created by zhandong on 2019-07-04.
 //
+#include "../src/rsa.h"
+#include <string>
+#include <fstream>
 
+
+
+int WriteString2FileAppend(const std::string &file_string, const std::string& str)
+{
+    std::ofstream OsWrite(file_string, std::ofstream::app);
+    OsWrite << str;
+    OsWrite << std::endl;
+    return 0;
+}
+
+
+int main(){
+    std::string ca_public_key, ca_private_key;
+    get_public_key(ca_public_key, ca_private_key);
+    WriteString2FileAppend("../keys.txt", ca_public_key);
+    WriteString2FileAppend("../keys.txt", ca_private_key);
+    WriteString2FileAppend("../keys.txt", "");
+    for(int i=0;i<2;i++) {
+        std::string public_key, private_key;
+        get_public_key(public_key, private_key);
+        WriteString2FileAppend("../keys.txt", public_key);
+        WriteString2FileAppend("../keys.txt", private_key);
+        WriteString2FileAppend("../keys.txt", "");
+        std::string result = rsa_encrypt(ca_public_key, public_key);
+        WriteString2FileAppend("../keys.txt", public_key +"_"+ result);
+        WriteString2FileAppend("../keys.txt", "");
+    }
+
+}
